13d9fb122c2a797647c7842deb99e9bf752b6f66
bugbit <ohernandezba@gmail.com>
2016-10-23 03:03:48 +0200

    resolver

--- CIFRAS.C
+++ CIFRAS.C
@@ -41,46 +41,23 @@
 
 enum EOperaciones
 {
-	Suma=0,multiplicacion,Resta,division
+	Suma=0,Multiplicacion,Resta,Division,None
 };
 
-typedef struct
+typedef struct _Expr2Bin_
 {
-	union
-	{
-		struct
-		{
-			unsigned num1:10;
-			unsigned num2:10;
-			unsigned operacion:2;
-		} s;
-		long n;	
-	} u;
+	int num1,num2;
+	enum EOperaciones operacion;
+	struct _Expr2Bin_ *next;
 } Expr2Bin;
 
 typedef struct
 {
-	unsigned char count;
-	void *expr2bin;
-} Expr2Bins;
-
-typedef struct __Combinaciones__
-{
-	unsigned char numeros[6];
-	union
-	{
-		struct
-		{
-			unsigned count:3;
-			unsigned idx1:3;
-			unsigned idx2:3;
-			unsigned operacion:2;
-		} s;
-		short n;
-	} u;
-	struct __Combinaciones__ *next;
-	Expr2Bins exprs;
-} Combinaciones;
+	int numeros[6];
+	int count;
+	int sum;
+	Expr2Bin solucion;
+} Solucion;
 
 int tv1grupos[4];
 int mNumeros[6];
@@ -92,9 +69,13 @@ int mgrupos[][6]=
 	{ 4,5,6,7,8,9 },
 	{ 10,10,25,50,75,100 }
 };
-int mCalcAprox;
-
-Combinaciones *mCombTop=NULL;
+Expr2Bin *mSolucion;
+int mObjetivoSol;
+enum EOperaciones 
+	mOpApx[]={ Multiplicacion,Suma,None },
+	mOpExc[]={ Multiplicacion,Suma,Resta,Division,None };
+	
+int FASTCALL resolsol(Solucion *sol,int aproxcalc,enum EOperaciones *op,int idxi,int idxj);
 
 void mostrarinstrucciones()
 {
@@ -291,115 +272,263 @@ int leerargs(int num,char **argv)
 	return -1;
 }
 
-void esaprox()
+int FASTCALL isexpr2binbottom(Expr2Bin *expr)
 {
-	// comparar si el cuadrado del sumatorio de las pistas es menor que el doble del número a hallar
-	
-	int s=0;
-	int i=0;
-	
-	for (;i<6;i++)
-		s += mNumeros[i];
+	return !expr->num1 && expr->num1==expr->num2;
+}
+
+void FASTCALL setexpr2binbottom(Expr2Bin *expr)
+{
+	expr->num1=expr->num2=0;
+	expr->next=NULL;
+}
+
+Expr2Bin * FASTCALL getexprfirst(Expr2Bin *expr)
+{
+	if (expr==NULL || isexpr2binbottom(expr) || expr->next==NULL)
+		return NULL;
 		
-	mCalcAprox= s*s<2*mObjetivo;
+	return expr->next->next;
 }
 
-void *mallocexprs(size_t size,int num)
+/*
+	Devuelve el calculo o -1 si no se puede calcular.
+	En las operaciones de resta o division se puede cambiar los operandos si es mayor
+	Si es una resta y el resultado es cero devolvemos -1
+	Si es una división y el resultado no es exacto devolvemos -1
+ */
+int FASTCALL calcexpr(Expr2Bin *expr)
 {
-	return malloc(size+num*sizeof(Expr2Bin));
-}
-
-void copythenumeros(Combinaciones *comb)
-{
-	int i,j,tmp;
+	int num1=expr->num1;
+	int num2=expr->num2;
+	div_t d;
 	
-	for(i=0;i<6;i++)
+	if (num1<num2)
 	{
-		comb->numeros[i]=mNumeros[i];
-		for (j=i-1;j>=0;i--)
+		expr->num1=num2;
+		expr->num2=num1;
+		num1=expr->num1;
+		num2=expr->num2;
+	}
+	switch(expr->operacion)
+	{
+		case Suma:
+			return num1+num2;
+		case Multiplicacion:
+			return num1*num2;
+		case Resta:
+			if (num1==num2)
+				return -1;			
+			
+			return expr->num1-expr->num2;
+		case Division:
+			d=div(num1,num2);
+			
+			return (d.rem) ? -1 : d.quot;				
+	}
+	
+	return -1;
+}
+
+void FASTCALL freeexpr(Expr2Bin *expr)
+{
+	Expr2Bin *expr2=getexprfirst(expr),*e;
+	
+	if (expr2==NULL)
+		return;
+	
+	do
+	{
+		e=expr2->next;
+		free(expr2);
+	} while (e!=NULL && !isexpr2binbottom(e));
+}
+
+Expr2Bin *FASTCALL cloneexpr(Expr2Bin *expr)
+{
+	Expr2Bin *exprnew,*exprn2,*e;
+	
+	if (expr==NULL)
+		return NULL;
+		
+	exprnew=NULL;
+	do
+	{
+		e=(Expr2Bin *) malloc(sizeof(Expr2Bin));
+		
+		if (e==NULL)
 		{
-			if (comb->numeros[j]>comb->numeros[j+1])
+			freeexpr(exprnew);
+			
+			return NULL;
+		}
+		*e=*expr;
+		if (exprnew==NULL)
+			exprnew=e;
+		else
+			exprn2->next=e;
+		exprn2=e;
+		expr=expr->next;
+	} while (expr!=NULL && !isexpr2binbottom(exprn2));
+	e->next=exprnew;
+	
+	return e;
+}
+
+/*
+ * Devuelve 1 si encontrada solucion, o si no, -1 si hay error
+ */
+int FASTCALL esenconsol(Expr2Bin *expr,int c)
+{
+	if (c==mObjetivo || abs(c-mObjetivoSol)>abs(c-mObjetivo))
+	{
+		freeexpr(mSolucion);
+		if ((mSolucion=cloneexpr(expr))==NULL)
+			return -1;
+			
+		mObjetivoSol=c;
+		
+		return (c==mObjetivo) ? 1 : 0;
+	}
+	
+	return 0;
+}
+
+void FASTCALL copynumerosasol(Solucion *sol,int count,int *numeros)
+{
+	int i,j,tmp,sum;
+	
+	sol->count=count;
+	for(i=0,sum=0;i<count;i++)
+	{
+		sum += numeros[i];
+		sol->numeros[i]=numeros[i];
+		for (j=i-1;j>=0;j--)
+		{
+			if (sol->numeros[j]<sol->numeros[j+1])
 			{
-				tmp=comb->numeros[j];
-				comb->numeros[j]=comb->numeros[j+1];
-				comb->numeros[j+1]=tmp;
+				tmp=sol->numeros[j];
+				sol->numeros[j]=sol->numeros[j+1];
+				sol->numeros[j+1]=tmp;
 			}
 			else
 				break;
 		}
 	}
+	sol->sum=sum;
 }
 
-Combinaciones *firstcomb()
+void FASTCALL initsolucion(Solucion *sol)
 {
-	Combinaciones *comb=(Combinaciones *) mallocexprs(sizeof(Combinaciones),1);
-	
-	if (comb!=NULL)
-	{
-		copythenumeros(comb);
-		comb->u.s.count=6;
-		comb->u.s.idx1=0;
-		comb->u.s.idx2=1;
-		comb->u.s.operacion=0;
-		comb->exprs.count=0;
-	}
-	
-	return comb;
+	setexpr2binbottom(&sol->solucion);
 }
 
-void pushcomb(Combinaciones *comb)
+bool FASTCALL esaprox(Solucion *sol)
 {
-	if (mCombTop!=NULL)
-		comb->next=mCombTop;
-	mCombTop=comb;
+	// comparar si el cuadrado del sumatorio de las pistas es menor que el doble del número a hallar
+	
+	return sol->sum*sol->sum<2*mObjetivo;
 }
 
-Combinaciones *popcomb()
+/*
+ * Devuelve 1 si encontrada solucion, o si no, -1 si hay error
+ */
+int FASTCALL initsol2op(Solucion *sdest,Solucion *ssource,enum EOperaciones op,int idx1,int idx2)
 {
-	Combinaciones *comb=mCombTop;
+	int i,c,r;
 	
-	if (comb!=NULL)
-		mCombTop=mCombTop->next;
+	sdest->sum=ssource->sum-ssource->numeros[idx1]-ssource->numeros[idx2];
+	for (i=sdest->count=0;i<ssource->count;i++)
+		if (i!=idx1 && i!=idx2)
+			sdest->numeros[sdest->count++]=ssource->numeros[i];
+	sdest->solucion.num1=ssource->numeros[idx1];
+	sdest->solucion.num2=ssource->numeros[idx2];
+	sdest->solucion.operacion=op;
 	
-	return comb;
-}
-
-void resetcombs()
-{
-	Combinaciones *comb;
+	/*
+		zero
+		1-zero-1
+		1-2-zero-1
+		1-2-3-zero-1
+	 */
 	
-	while ((comb=popcomb())!=NULL)
-		free(comb);
-}
-
-//int op(Expr2Bin *expr,int *)
-
-void FASTCALL resolcomb(Combinaciones *comb)
-{
-	Expr2Bin *expr=((Expr2Bin *)&comb->exprs.expr2bin)+comb->exprs.count;
+	if (isexpr2binbottom(&ssource->solucion))
+		ssource->solucion.next=&sdest->solucion;
+	sdest->solucion.next=&ssource->solucion;
+	c=calcexpr(&sdest->solucion);
 	
-	comb->exprs.count++;
-	expr->u.s.num1=comb->numeros[comb->u.s.idx1];
-	expr->u.s.num2=comb->numeros[comb->u.s.idx2];
-}
-
-void resolver()
-{
-	Combinaciones *comb,*comb1;
-	
-	if ((comb1=comb=firstcomb())==NULL)
-	{
-		puts(strerror(errno));
+	if (c<0)
+		return -1;
 		
-		return;
+	r=esenconsol(&sdest->solucion,c);
+	if (r)
+		return r;
+	
+	sdest->numeros[sdest->count++]=c;
+	sdest->sum += c;
+	
+	return 0;
+}
+
+/*
+ * Devuelve 1 si encontrada solucion, o si no, -1 si hay error
+ */
+int FASTCALL resolsol2op(Solucion *_sol,int aproxcalc,enum EOperaciones *op,int idx1,int idx2)
+{
+	Solucion sol;
+	int j,r;
+	
+	if (initsol2op(&sol,_sol,*op,idx1,idx2))
+		return 0;
+		
+	return resolsol(&sol,aproxcalc,op,idx1,idx2+1);
+}
+
+/*
+ * Devuelve 1 si encontrada solucion, o si no, -1 si hay error
+ */
+int FASTCALL resolsol(Solucion *sol,int aproxcalc,enum EOperaciones *_op,int idxi,int idxj)
+{
+	int isaprox=esaprox(sol),i,j,r;
+	enum EOperaciones *op=(_op!=NULL) ? _op : (isaprox) ? mOpApx : mOpExc;
+	
+	if (!aproxcalc && isaprox)
+		return 0;
+	
+	if (idxj!=0)
+	{
+		for (j=idxj;j<sol->count;j++)
+			if ((r=resolsol2op(sol,aproxcalc,op,i,j)))
+				return r;
+		idxi++;
 	}
-	esaprox();
-	//pushcomb(comb1);
-	do
-		resolcomb(comb);
-	while ((comb=popcomb())!=NULL);
-	resetcombs();
-	free(comb1);
+	for(;*op!=None;op++)
+	{
+		for (i=idxi;i<sol->count;i++)
+		{
+			for (j=i+1;j<sol->count;j++)
+				if ((r=resolsol2op(sol,aproxcalc,op,i,j)))
+					return r;
+		}
+		idxi=0;
+	}
+	
+	return 0;
+}
+
+/*
+ * Devuelve 1 si encontrada solucion, o si no, -1 si hay error
+ */
+int FASTCALL resolver()
+{
+	Solucion solucion;
+	
+	initsolucion(&solucion);
+	copynumerosasol(&solucion,6,(int*) &mNumeros);
+	mSolucion=NULL;
+	mObjetivoSol=0;
+	
+	return resolsol(&solucion,esaprox(&solucion),NULL,0,0);
 }
 
 void splitargs(char *linea,int *num,char **args)
